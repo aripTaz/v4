03201cde22ce206b5d2b8191b2d8bb252abc0ba4
aripTaz <121712805+aripTaz@users.noreply.github.com>
2023-10-15 00:43:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,17 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
+# Fungsi untuk mengirim notifikasi menggunakan Telegram Bot API
+send_notification() {
+    chat_id="6425014714"
+    token="6491411196:AAG7hWWkVnLefrKaHzh8YDG5FIxWqcdLuMU"
+    message="$1"
 
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d text="$message" \
+        -d parse_mode="HTML"
+}
 clear
 red='\e[1;31m'
 green='\e[0;32m'
@@ -263,7 +273,7 @@ rm /root/setup.sh >/dev/null 2>&1
 rm /root/ins-xray.sh >/dev/null 2>&1
 rm /root/insshws.sh >/dev/null 2>&1
 secs_to_human "$(($(date +%s) - ${start}))" | tee -a log-install.txt
-echo -e "
-"
+echo -e ""
+send_notification "Instalasi bash full selesai."
 read -n 1 -s -r -p "Press enter to reboot"
 reboot
